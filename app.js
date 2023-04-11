aef6cb7e235407556133ec1d8a414230b5b1a4c3
Shay Ellis <58398990+ShayEllis@users.noreply.github.com>
2023-04-10 17:57:11 -0700

    finished project

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 /* 
  * Package Imports
 */
-
 const path = require("path");
 require("dotenv").config();
 const express = require('express');
 const partials = require('express-partials');
+
+const session = require('express-session')
+const passport = require('passport')
+const GitHubStrategy = require('passport-github2').Strategy
 
 
 const app = express();
@@ -23,11 +26,22 @@ const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
  * Passport Configurations
 */
 
+passport.use(new GitHubStrategy({
+  clientID: GITHUB_CLIENT_ID,
+  clientSecret: GITHUB_CLIENT_SECRET,
+  callbackURL: 'http://localhost:3000/auth/github/callback'
+}, (accessToken, refreshToken, profile, done) => {
+  done(null, profile)
+}))
 
 
+passport.serializeUser((user, done) => {
+  done(null, user)
+})
 
-
-
+passport.deserializeUser((user, done) => {
+ done(null, user)
+})
 
 /*
  *  Express Project Setup
@@ -39,8 +53,14 @@ app.use(partials());
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
 
+app.use(session({
+  secret: 'codecademy',
+  resave: false,
+  saveUninitialized: false
+}))
 
-
+app.use(passport.initialize())
+app.use(passport.session())
 
 /*
  * Routes
@@ -50,7 +70,7 @@ app.get('/', (req, res) => {
   res.render('index', { user: req.user });
 })
 
-app.get('/account', (req, res) => {
+app.get('/account', ensureAuthenticated, (req, res) => {
   res.render('account', { user: req.user });
 });
 
@@ -63,8 +83,12 @@ app.get('/logout', (req, res) => {
   res.redirect('/');
 });
 
+app.get('/auth/github', passport.authenticate('github', { scope: ['user'] }))
 
-
+app.get('/auth/github/callback', passport.authenticate('github', { 
+  failureRedirect: '/login', 
+  successRedirect: '/'
+}))
 
 /*
  * Listener
@@ -76,3 +100,9 @@ app.listen(PORT, () => console.log(`Listening on ${PORT}`));
  * ensureAuthenticated Callback Function
 */
 
+function ensureAuthenticated (req, res, next) {
+  if (!req.isAuthenticated()) {
+    res.redirect('/login')
+  }
+  next()
+}
